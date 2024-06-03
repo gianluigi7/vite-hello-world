191a5e8f93be8c9114de1b7e29913daa4c58791d
gianluigi7 <gianluigiferraioli7@gmail.com>
2024-06-03 16:15:24 +0200

    .

---
+++ src/App.vue
@@ -0,0 +1,16 @@
+<script>
+  import AppTitle from './components/AppTitle.vue';
+
+  export default {
+    name: 'MyApp',
+    components: {
+      AppTitle
+    }
+  }
+</script>
+
+<script></script>
+<template>
+ <AppTitle/>
+</template>
+<style scoped></style>
